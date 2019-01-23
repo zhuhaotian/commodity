6c738ed6508adf9d613c079ce187483125957cb0
yu <2074225252@qq.com>
2019-01-23 11:10:41 +0800

    此项目已经升级为生产者

--- src/main/java/com/jk/ProducerCommodityApplication.java
+++ src/main/java/com/jk/ProducerCommodityApplication.java
@@ -3,9 +3,10 @@ package com.jk;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient//声明此项目是一个生产者
 @MapperScan("com.jk.mapper")
 public class ProducerCommodityApplication {
 
