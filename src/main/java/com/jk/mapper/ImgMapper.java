1601684e1243bea3f6dd823c8c42a6cec55c9779
“zht <2865955965@qq.com>
2019-01-23 21:06:12 +0800

    朱浩天的提交

---
+++ src/main/java/com/jk/mapper/ImgMapper.java
@@ -0,0 +1,9 @@
+package com.jk.mapper;
+
+import com.jk.bean.ImgBean;
+
+import java.util.List;
+
+public interface ImgMapper {
+    List<ImgBean> ImgCon(ImgBean imgBean);
+}
---
+++ src/main/java/com/jk/service/ImgService.java
@@ -0,0 +1,10 @@
+package com.jk.service;
+
+import com.jk.bean.ImgBean;
+
+import java.util.List;
+
+public interface ImgService {
+
+    List<ImgBean> ImgCon(ImgBean imgBean);
+}
